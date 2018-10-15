83b1a3de2bbac2f6cafaf206ea583b3e1a923680
tepextepex <tepextepex@gmail.com>
2018-10-15 15:37:03 +0300

    Better example for 'agent' rabbit

--- test_agent.py
+++ test_agent.py
@@ -2,4 +2,7 @@ from rabbit_niersc import Rabbit
 
 r = Rabbit(host="localhost", profession="agent", name="test", data_type="test_data")
 
+# notice that we didn't specify any functions for r.duty method
+# so every time this rabbit receives a message it will notice us that it has nothing to do
+
 r.run()
