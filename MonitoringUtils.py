d74d98f7ad4f5040cf1d856fb1f62557d04ebee7
tepextepex <tepextepex@gmail.com>
2018-10-30 14:53:35 +0300

    Added logging function

--- MonitoringUtils.py
+++ MonitoringUtils.py
@@ -3,6 +3,7 @@ import os.path
 from datetime import datetime
 import psycopg2 as psy
 import glob
+import json
 
 
 def seek_new_products(source_dir, pattern, host, dbname, login, pwd, db_table):
@@ -21,7 +22,7 @@ def seek_new_products(source_dir, pattern, host, dbname, login, pwd, db_table):
         for fname in names:
             if not is_in_db(cur, fname, db_table):
                 new.append(fname)
-                insert_in_db(cur, fname, db_table)  # TODO: should we insert a new product in a DB right now?
+                insert_in_db(cur, fname, db_table)  # TODO: should we insert a new product in a DB right now..?
     else:
         print "Can't find directory: %s" % source_dir
     cur.close()
@@ -44,8 +45,29 @@ def insert_in_db(cursor, fname, db_table):
     """
     Inserts a specified filename in the database.
 
-    :return: 1 if fname was successfully inserted or -1 if was not
+    :returns: 1 if fname was successfully inserted or -1 if was not
     """
     timestamp = datetime.now().timestamp()
     cursor.execute("INSERT INTO %s (product_id, timestamp) VALUES ('%s', '%s')" % (db_table, fname, timestamp))
     return cursor.rowcount
+
+
+def log(who, message, host, dbname, login, pwd, db_table):
+    """
+    Insert the JSON message into database containing log.
+
+    :returns:  1 if the message was successfully logged or -1 if it was not
+    """
+    conn_string = "host=%s dbname=%s user=%s password=%s" % (host, dbname, login, pwd)
+    conn = psy.connect(conn_string)
+    cur = conn.cursor()
+
+    json_message = json.dumps(message)
+    timestamp = datetime.now().timestamp()
+
+    cur.execute("INSERT INTO %s (who, message, timestamp) VALUES ('%s', '%s', '%s')" % (db_table, who, json_message, timestamp))
+
+    cur.close()
+    conn.close()
+
+    return cur.rowcount
