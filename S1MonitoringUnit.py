a8b413d01ec80393b6a987e58853943372752427
tepextepex <tepextepex@gmail.com>
2018-11-02 17:45:46 +0300

    Finds Sentinel-1 zip-archives and writes metadata in DB

--- S1MonitoringUnit.py
+++ S1MonitoringUnit.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from auth import auth_data, host, vhost_name
 from auth import db_auth_data, db_host, db_name, db_table
 from auth import storage
+from S1L1Tools import S1L1Tools
 from MonitoringUtils import *  # allows using its functions without prefixing them with the module name. But be aware of namespace collisions!
 
 credentials = auth_data["cordelia"]
@@ -19,6 +20,17 @@ r = Rabbit(credentials=credentials,
 new_products = seek_new_products(storage, "S1*.zip", db_host, db_name, db_auth_data[0], db_auth_data[1], db_table)
 
 for product in new_products:
+    s = S1L1Tools(os.path.join(storage, product))
+    extent = s.metadata['foot_print']
+    geojson_name = os.path.join(storage, product + "_extent.geojson")
+    save_extent_as_geojson(extent, geojson_name)
+
+    # inserts metadata in DB:
+    with open(os.path.join(storage, product + "_extent.geojson")) as f:
+        data = json.load(f)
+        db_insert_geojson(db_host, db_name, db_auth_data[0], db_auth_data[1], db_table, product, "S1_zip", data)
+
+    # construct message and send it to the agents:
     payload = {
         "time": str(datetime.now().time()),
         "source_data": product,
